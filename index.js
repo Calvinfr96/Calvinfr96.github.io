83a5dee3f18daa2bf66b6889eb77a3d6c061f0a4
calvin <calvin.francis96@outlook.com>
2021-03-26 19:28:32 -0400

    Add index.js with array containing planned projects

---
+++ index.js
@@ -0,0 +1,8 @@
+const projects = [
+    { name: 'Project 1', description: 'Build and host a dynamic personal website using JavaScript events and DOM manipulation' },
+    { name: 'Project 2', description: 'Build a webpage that is dynamically populated with remote content from an API' },
+    { name: 'Project 3', description: 'Build a front-end application with content dynamically populated from an API using React' },
+    { name: 'Project 4', description: 'Design and create a Command Line Interface (CLI) application' },
+    { name: 'Project 5', description: 'Build a full-stack application that persists data through an API' },
+    { name: 'Project 6', description: 'Design and create a full-stack application using React and Rails API' }
+];
