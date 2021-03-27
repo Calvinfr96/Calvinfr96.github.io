5c0d7dfc2897f4ce4865b143ca33ac1e5a69c45c
calvin <calvin.francis96@outlook.com>
2021-03-26 20:20:34 -0400

    Create cards with project description and add to DOM

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const projects = [
     { name: 'Project 5', description: 'Build a full-stack application that persists data through an API' },
     { name: 'Project 6', description: 'Design and create a full-stack application using React and Rails API' }
 ];
+
+function createCardFromProject(project) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML =
+        `
+    <h3>${project.name}</h3>
+    <p>${project.description}</p>
+    `;
+    return card
+};
+
+const projectsContainer = document.getElementsByClassName('projects-container')[0];
+for (i = 0; i < projects.length; i++) {
+    card = createCardFromProject(projects[i]);
+    projectsContainer.appendChild(card);
+}
